d4d1c7b12cd36559adc57d275b9ae7964b19ca0e
SanKuanYoYi <1575424170@qq.com>
2025-10-19 21:56:14 +0800

    Add files via upload
    字符三角形

---
+++ 小型等腰三角形.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    
+	
+    char C;
+	scanf("%c",&C);
+    
+    printf("  %c\n",C  );
+    printf(" %c%c%c\n",C,C,C );
+    printf("%c%c%c%c%c\n",C,C,C,C,C ); 
+	
+	
+	
+	
+    
+    
+    
+    
+return 0;
+}

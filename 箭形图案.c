0fce7e19830e70291b69d41e5be1e5ae1cd7249b
SanKuanYoYi <1575424170@qq.com>
2025-10-22 17:11:21 +0800

    Add files via upload

---
+++ 箭形图案.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    
+    for (int i = 0; i <= n; i++) {
+        printf("%*s", 2 * (n - i), "");
+        for (int j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%*s", 2 * (n - i), "");
+        for (int j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}

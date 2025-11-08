f3b434b6238c2afaffa0f53441adec1714457a33
SanKuanYoYi <1575424170@qq.com>
2025-11-08 23:36:37 +0800

    Update 单词缩写.c

--- 单词缩写.c
+++ 单词缩写.c
@@ -17,7 +17,7 @@ int main(){
         num++;
     }
     
-    for(int i=1;str[i]!=EOF;i++){
+    for(int i=1;str[i]!='\0';i++){
         
         if(str[i]==' ' && str[i+1]!=' '){
             UpAlp[num]=str[i+1];
@@ -33,3 +33,4 @@ int main(){
     
     return 0;
 }
+

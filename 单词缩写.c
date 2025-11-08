0289ffc52cdbaed0545a0cb06cf1278db01572eb
SanKuanYoYi <1575424170@qq.com>
2025-11-08 23:33:59 +0800

    Add files via upload
    Word abbreviation

---
+++ 单词缩写.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <string.h>
+#include<ctype.h>
+
+int main(){
+    
+    char str[5098];
+    fgets(str,sizeof(str),stdin);
+    
+    char UpAlp[100];
+    UpAlp[0]=str[0];
+    
+    int num=0;
+    
+    if(str[0]!=' ' && str[0]!='\0'){
+        UpAlp[num]=str[0];
+        num++;
+    }
+    
+    for(int i=1;str[i]!=EOF;i++){
+        
+        if(str[i]==' ' && str[i+1]!=' '){
+            UpAlp[num]=str[i+1];
+            num++;
+        }
+    }
+    
+    for(int i=0;i<num;i++){
+        
+        UpAlp[i]=toupper(UpAlp[i]);
+        printf("%c",UpAlp[i]);
+    }
+    
+    return 0;
+}
